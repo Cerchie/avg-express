80ec5df4a3abae848c2bdc6917711e47e894b8c4
Lucia Cerchie <luciacerchie@gmail.com>
2020-10-20 11:07:09 -0700

    all routes up, time to test

--- app.js
+++ app.js
@@ -8,45 +8,56 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+function convertAndValidateNumsArray(numsAsStrings) {
+    let result = [];
+
+    for (let i = 0; i < numsAsStrings.length; i++) {
+        let valToNumber = Number(numsAsStrings[i]);
+
+        if (Number.isNaN(valToNumber)) {
+            return new Error(
+                `The value '${numsAsStrings[i]}' at index ${i} is not a valid number.`
+            );
+        }
+
+        result.push(valToNumber);
+    }
+    return result;
+}
 //method from stackoverflow
 app.get('/mean/:fnums', function (req, res) {
-    console.log(req.params.fnums)
-    const sum = req.params['fnums'].reduce((acc, cur) => acc + cur);
-    const average = sum / req.params['fnums'].length;
-    return res.json(`{
-        fnums: Avg ${average}
-    }`) //not sure why it would be json
+    let arr = convertAndValidateNumsArray(req.params.fnums)
+    console.log('consoleconsoleconsole')
+    const sum = arr.reduce((acc, cur) => acc + cur);
+    const average = (sum / arr.length);
+    console.log(average)
+    return res.json(`${average}`) //not sure why it would be json
 });
 // from https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundamental-exercise-88.php
 app.get('/median/:fnums', function (req, res) {
+    let arr = convertAndValidateNumsArray(req.params.fnums)
+    const mid = Math.floor(arr.length / 2),
+        nums = [...arr.sort((a, b) => a - b)];
+    const median = arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+    return res.json(`${median}`);
 
-    arr = req.params['fnums']
-    const median = arr => {
-        const mid = Math.floor(arr.length / 2),
-            nums = [...arr.sort((a, b) => a - b)];
-        arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
-    };
-    return res.json(`{        
-        ${req.params['fnums']} : ${median}
-    }`);
 });
 //from https://stackoverflow.com/questions/52898456/simplest-way-of-finding-mode-in-javascript
 app.get('/mode/:fnums', function (req, res) {
-    let a = req.params['fnums'];
-    const mode = a =>
-        Object.values(
-            a.reduce((count, e) => {
-                if (!(e in count)) {
-                    count[e] = [0, e];
-                }
-                count[e][0]++;
-                return count;
-            }, {})
-        ).reduce((a, v) => v[0] < a[0] ? a : v, [0, null])[1];
-    return res.json(`{
-        ${req.params['fnums']} : ${mode}
-    }`)
+    let a = convertAndValidateNumsArray(req.params.fnums)
+    let mode = Object.values(
+        a.reduce((count, e) => {
+            if (!(e in count)) {
+                count[e] = [0, e];
+            }
+            count[e][0]++;
+            return count;
+        }, {})
+    ).reduce((a, v) => v[0] < a[0] ? a : v, [0, null])[1];
+    return res.json(`${mode}`)
 });
+
+
 
 app.listen(3000, function () {
     console.log('App on port 3000');
