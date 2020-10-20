9007254d93a27e4201d0d78a7342bf570065792d
Lucia Cerchie <luciacerchie@gmail.com>
2020-10-20 11:52:54 -0700

    Starting on err msgs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const expressError = require("./error")
 const app = express();
 
 //Tell Express to parse request bodies for either form data or JSON:
@@ -57,7 +57,25 @@ app.get('/mode/:fnums', function (req, res, next) {
     return (`response: operation: "median", value:${mode}`)
 });
 
+app.use(function (req, res, next) {
+    const notFoundError = new ExpressError("Not Found", 404);
+    return next(notFoundError)
+});
 
+// generic error handler
+app.use(function (err, req, res, next) {
+    // the default status is 500 Internal Server Error
+    let status = err.status || 500;
+    let message = err.message;
+
+    // set the status and alert the user
+    return res.status(status).json({
+        error: {
+            message,
+            status
+        }
+    });
+});
 
 app.listen(3000, function () {
     console.log('App on port 3000');
